3de2c13c6e19093adebcf17e0510c4322d1ae1ba
minjun <ijmtylhtf@naver.com>
2023-08-02 19:49:06 +0900

    [Chore] 파일 이름 변경

--- ModernJavaInAction/src/스트림/ch5/Filtering.java
+++ ModernJavaInAction/src/스트림/ch5/Filtering.java
@@ -4,10 +4,9 @@ import static java.util.stream.Collectors.toList;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import 스트림.ch4.Dish;
 
-public class BuildingStreams {
+public class Filtering {
 
     public static void main(String[] args) {
         System.out.println("Filtering with predicate");
@@ -17,7 +16,7 @@ public class BuildingStreams {
         vegetarianMenu.forEach(System.out::println);
 
 
-        //distinct 사용 : 고유 요소 거름 
+        //distinct 사용 : 고유 요소 거름
         System.out.println("Filtering unique elements:");
         List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
         numbers.stream()
