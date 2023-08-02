5d649621f1b6aed201fe91c3b4ba68ba92b93066
minjun <ijmtylhtf@naver.com>
2023-08-02 19:53:16 +0900

    [Feature] 스페셜 메뉴 칼로리 320 이하 필터링

--- ModernJavaInAction/src/스트림/ch5/Filtering.java
+++ ModernJavaInAction/src/스트림/ch5/Filtering.java
@@ -4,6 +4,7 @@ import static java.util.stream.Collectors.toList;
 
 import java.util.Arrays;
 import java.util.List;
+import org.w3c.dom.ls.LSOutput;
 import 스트림.ch4.Dish;
 
 public class Filtering {
@@ -15,13 +16,29 @@ public class Filtering {
                 .collect(toList());
         vegetarianMenu.forEach(System.out::println);
 
-
         //distinct 사용 : 고유 요소 거름
         System.out.println("Filtering unique elements:");
         List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
         numbers.stream()
-                .filter(i->i%2==0)
+                .filter(i -> i % 2 == 0)
                 .distinct()
                 .forEach(System.out::println);
+
+        //스트림 슬라이스
+        List<Dish> specialMenu = Arrays.asList(
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER));
+        System.out.println("Filtered sorted menu");
+        List<Dish> filteredMenu = specialMenu.stream()
+                .filter(dish -> {
+                    return dish.getCalories() < 320;
+                })
+                .collect(toList());
+
+
     }
+
 }
