e36c4dd5a8cda9038b057e12743bfa4011019fb4
minjun <ijmtylhtf@naver.com>
2023-08-04 17:22:29 +0900

    [Feat] noneMatch 사용

--- ModernJavaInAction/src/스트림/ch5/Finding.java
+++ ModernJavaInAction/src/스트림/ch5/Finding.java
@@ -10,6 +10,7 @@ public class Finding {
         }
         System.out.println("isVegetarianFriendlyMenu() = " + isVegetarianFriendlyMenu());
         System.out.println("isHealthyMenu() = " + isHealthyMenu());
+        System.out.println("isHealthyMenu2() = " + isHealthyMenu2());
 
     }
 
@@ -21,5 +22,10 @@ public class Finding {
     //allMatch : 모든 요소와 일치하는지
     private static boolean isHealthyMenu(){
         return Dish.menu.stream().allMatch(d->d.getCalories() < 1000);
+    }
+    
+    //noneMatch : 하나도 일치하는 요소가 없는 지 
+    private static boolean isHealthyMenu2(){
+        return Dish.menu.stream().noneMatch(d->d.getCalories()>=1000);
     }
 }
