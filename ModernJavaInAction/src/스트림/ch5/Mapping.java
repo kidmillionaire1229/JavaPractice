4cc8f3090080a7e157a33ad58e5d8f2d01e48c42
minjun <ijmtylhtf@naver.com>
2023-08-04 15:10:21 +0900

    [Feat] Map함수 이용하여 리스트 내 글자 길이 추출 후 반환

--- ModernJavaInAction/src/스트림/ch5/Mapping.java
+++ ModernJavaInAction/src/스트림/ch5/Mapping.java
@@ -2,6 +2,7 @@ package 스트림.ch5;
 
 import static java.util.stream.Collectors.toList;
 
+import java.util.Arrays;
 import java.util.List;
 import 스트림.ch4.Dish;
 
@@ -16,7 +17,12 @@ public class Mapping {
 
         System.out.println("dishNames = " + dishNames);
 
-        
+        //map함수 이용하여 글자의 길이들로 추출 후 변환
+        List<String> words = Arrays.asList("Modern", "Java", "In", "Action");
+        List<Integer> wordsLengths = words.stream()
+                .map(String::length)
+                .collect(toList());
+        System.out.println("wordsLengths = " + wordsLengths);
     }
 
 }
