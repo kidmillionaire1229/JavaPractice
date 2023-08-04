0008e809822df201cad36d45d03bf7b1d590b04f
minjun <ijmtylhtf@naver.com>
2023-08-04 17:19:17 +0900

    [Feat] alMatch 사용

--- ModernJavaInAction/src/스트림/ch5/Finding.java
+++ ModernJavaInAction/src/스트림/ch5/Finding.java
@@ -8,10 +8,18 @@ public class Finding {
         if(isVegetarianFriendlyMenu()){
             System.out.println("Vegatarian Friendly");
         }
+        System.out.println("isVegetarianFriendlyMenu() = " + isVegetarianFriendlyMenu());
+        System.out.println("isHealthyMenu() = " + isHealthyMenu());
+
     }
 
     //anyMatch : 적어도 한 요소와 일치하는지 확인
     private static boolean isVegetarianFriendlyMenu(){
         return Dish.menu.stream().anyMatch(Dish::isVegetarian);
+    }
+
+    //allMatch : 모든 요소와 일치하는지
+    private static boolean isHealthyMenu(){
+        return Dish.menu.stream().allMatch(d->d.getCalories() < 1000);
     }
 }
