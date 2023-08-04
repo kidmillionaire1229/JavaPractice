24c7910d5286b0f3977e8707c0253c0e4ff3ab68
minjun <ijmtylhtf@naver.com>
2023-08-04 15:05:40 +0900

    [Feat] map함수 사용하여 dish들의 이름들을 mapping하여 반환

---
+++ ModernJavaInAction/src/스트림/ch5/Mapping.java
@@ -0,0 +1,18 @@
+package 스트림.ch5;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+import 스트림.ch4.Dish;
+
+public class Mapping {
+
+    public static void main(String[] args) {
+
+        //map함수 사용하여 Dish의 이름 추출 => List<String> 
+        List<String> dishNames = Dish.menu.stream()
+                .map(Dish::getName)
+                .collect(toList());
+    }
+
+}
