c213e59558068cbe1a908fa4fece1e85fb3c85d5
minjun <ijmtylhtf@naver.com>
2023-08-04 16:24:53 +0900

    [Feat] flatMap사용 순서쌍 반환

--- ModernJavaInAction/src/스트림/ch5/Mapping.java
+++ ModernJavaInAction/src/스트림/ch5/Mapping.java
@@ -4,6 +4,7 @@ import static java.util.stream.Collectors.toList;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 import 스트림.ch4.Dish;
 
 public class Mapping {
@@ -32,8 +33,9 @@ public class Mapping {
                 .collect(toList());
 
 
+        //flatmap사용 1 (String 배열)
         //Arrays::strem은 String[] -> Stream<String>
-        //Stream<Stream<String>> -> Stream<String> 
+        //Stream<Stream<String>> -> Stream<String>
         List<String> uniqueCharacters = words.stream()
                 .map(word -> word.split(""))
                 .flatMap(Arrays::stream)
@@ -41,6 +43,18 @@ public class Mapping {
                 .collect(toList());
 
         System.out.println("uniqueCharacters = " + uniqueCharacters);
+
+        //flatmap사용 2 (Integer 배열)
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+        List<Integer> numbers2 = Arrays.asList(3,4);
+
+
+        //[(1,3),(1,4),...와 같이 숫자 쌍으로 반환
+        // i를 stream<int[]>로 반환하니까 그걸 flatMap으로 평면화 시켜야함 
+        numbers1.stream()
+                .flatMap(i-> numbers2.stream()
+                        .map(j->new int[]{i,j}))
+                .collect(toList());
     }
 
 }
