33af10dbaca8ba5eb83876a4675dc2a151dc0a09
minjun <ijmtylhtf@naver.com>
2023-08-04 17:30:39 +0900

    [Feat] findAny 함수 사용

--- ModernJavaInAction/src/스트림/ch5/Finding.java
+++ ModernJavaInAction/src/스트림/ch5/Finding.java
@@ -1,5 +1,6 @@
 package 스트림.ch5;
 
+import java.util.Optional;
 import 스트림.ch4.Dish;
 
 public class Finding {
@@ -11,6 +12,7 @@ public class Finding {
         System.out.println("isVegetarianFriendlyMenu() = " + isVegetarianFriendlyMenu());
         System.out.println("isHealthyMenu() = " + isHealthyMenu());
         System.out.println("isHealthyMenu2() = " + isHealthyMenu2());
+        findAnyVegetarian();
 
     }
 
@@ -27,5 +29,15 @@ public class Finding {
     //noneMatch : 하나도 일치하는 요소가 없는 지 
     private static boolean isHealthyMenu2(){
         return Dish.menu.stream().noneMatch(d->d.getCalories()>=1000);
+    }
+
+    //검색
+    //findAny: 임의의 요소 반환 (가장 먼저 탐색) 
+    private static void findAnyVegetarian(){
+                Dish.menu.stream()
+                        .filter(Dish::isVegetarian)
+                        .findAny()
+                        .ifPresent(dish-> System.out.println("find Any dish = " + dish));
+
     }
 }
