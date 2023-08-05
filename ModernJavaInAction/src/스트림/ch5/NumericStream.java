2b5c12e044941a3c07b197628518aa57072d5c2a
minjun <ijmtylhtf@naver.com>
2023-08-05 18:15:40 +0900

    [Feat] 피타고라스 예제 1

--- ModernJavaInAction/src/스트림/ch5/NumericStream.java
+++ ModernJavaInAction/src/스트림/ch5/NumericStream.java
@@ -50,6 +50,13 @@ public class NumericStream {
         IntStream evenNumbers2 = IntStream.range(1, 100)
                 .filter(n -> n % 2 == 0);
         System.out.println("range 사용"+evenNumbers2.count());
+
+        //피타고라스 수 예제
+        Stream<int[]> pythagoreanTriples = IntStream.rangeClosed(1, 100).boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                        .filter(b -> Math.sqrt(a * a + b * b) % 1 == 0).boxed()
+                        .map(b -> new int[]{a, b, (int) Math.sqrt(a * a + b * b)}));
+        pythagoreanTriples.forEach(t-> System.out.println(t[0]+","+t[1]+","+t[2]));
     }
 
 }
