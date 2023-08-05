942b606168bcb62148c4454245771d6224f23c95
minjun <ijmtylhtf@naver.com>
2023-08-05 15:41:49 +0900

    [Feat] reduce 사용 (메서드 참조)

--- ModernJavaInAction/src/스트림/ch5/Reducing.java
+++ ModernJavaInAction/src/스트림/ch5/Reducing.java
@@ -1,5 +1,17 @@
 package 스트림.ch5;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Reducing {
 
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(3, 4, 5, 1, 2);
+        int sum = numbers.stream().reduce(0, (a, b) -> a + b);
+        System.out.println("sum = " + sum);
+
+        //메서드 참조를 인자로 
+        Integer sum2 = numbers.stream().reduce(0, Integer::sum);
+        System.out.println("sum2 = " + sum2);
+    }
 }
