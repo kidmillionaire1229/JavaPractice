381d8dce3694160767e148096dbac47c3a95dfc0
minjun <ijmtylhtf@naver.com>
2023-08-10 20:37:27 +0900

    [Feat] 칼로리의 범위에 따라 칼로리 수준을 enum으로 분류하여 처리

--- ModernJavaInAction/src/스트림/ch6/Grouping.java
+++ ModernJavaInAction/src/스트림/ch6/Grouping.java
@@ -9,12 +9,24 @@ import 스트림.ch4.Dish;
 
 public class Grouping {
 
+    enum CaloricLevel {DIET,NORMAL,FAT};
+
     public static void main(String[] args) {
         System.out.println("Dishes grouped by type: "+groupDishesByType());
+        System.out.println("Dishes grouped by caloric level: "+groupDishesByCaloricLevel());
     }
 
     private static Map<Dish.Type, List<Dish>> groupDishesByType(){
         return Dish.menu.stream().collect(groupingBy(Dish::getType));
+    }
+
+    private static Map<CaloricLevel, List<Dish>> groupDishesByCaloricLevel(){
+        return Dish.menu.stream()
+                .collect(groupingBy(dish->{
+                        if(dish.getCalories()<=400) return CaloricLevel.DIET;
+                        else if(dish.getCalories()<=700) return CaloricLevel.NORMAL;
+                        else return CaloricLevel.FAT;
+                }));
     }
 
 }
