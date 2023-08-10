5654fee3d5b94fc542912920b20337db9fd8d008
minjun <ijmtylhtf@naver.com>
2023-08-10 20:21:33 +0900

    [Feat] 리듀싱 이용해서 문자열 조인 문제

--- ModernJavaInAction/src/스트림/ch6/Reducing.java
+++ ModernJavaInAction/src/스트림/ch6/Reducing.java
@@ -2,6 +2,7 @@ package 스트림.ch6;
 
 import static java.util.stream.Collectors.reducing;
 
+import java.util.stream.Collectors;
 import 스트림.ch4.Dish;
 
 public class Reducing {
@@ -24,8 +25,22 @@ public class Reducing {
     }
 
     //기본적인 Intstream::sum함수 사용
-    //정수 합계 기준 best practice : 간결하고 가독성이 높음 + 자동 언박싱 연산 수행 
+    //정수 합계 기준 best practice : 간결하고 가독성이 높음 + 자동 언박싱 연산 수행
     private static int calculateTotalCaloriesUsingSum(){
         return Dish.menu.stream().mapToInt(Dish::getCalories).sum();
     }
+
+    /**
+     * 리듀싱으로 문자열 연결하기 예제**/
+    //Dish.menu.stream().map(Dish::getName).collect(joining()) 연산을 리듀싱으로 구현해보자
+    private static void makeStringJoinWithReducing(){
+        String shortMenu1 = Dish.menu.stream().map(Dish::getName).
+                collect(reducing((s1, s2) -> s1 + s2)).get();
+        String shortMenu2 = Dish.menu.stream()
+                .collect(reducing("", Dish::getName, (s1, s2) -> s1 + s2));
+        //잘못된 예제 : reducing은 BiFunction<R,R,R>을 인수로 받는다.
+        //Dish.menu.stream().collect(reducing((d1,d2)->d1.getName()+d2.getName())).get();
+
+    }
+
 }
