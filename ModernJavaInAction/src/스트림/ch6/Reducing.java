922a34d67ca16d276865703ba7d8b4a77ea5873f
minjun <ijmtylhtf@naver.com>
2023-08-10 20:05:35 +0900

    [Feat] reducing 함계함수에 함수 참조자인 Integer::sum을 사용

--- ModernJavaInAction/src/스트림/ch6/Reducing.java
+++ ModernJavaInAction/src/스트림/ch6/Reducing.java
@@ -8,11 +8,17 @@ public class Reducing {
 
     public static void main(String[] args) {
         System.out.println("Total Calories in menu: "+calculateTotalCalories());
+        System.out.println("Total Calories in menu: "+calculateTotalCaloriesWithMethodReference());
     }
 
     //Collectors.reducing : (초기값, 변환 함수, 합계함수)
     private static int calculateTotalCalories(){
         return Dish.menu.stream().collect(reducing(0,Dish::getCalories,
                 (Integer i, Integer j)->i+j));
+    }
+
+    //Collectors.reducing -> 합계함수에 Integer::sum 사용하기
+    private static int calculateTotalCaloriesWithMethodReference(){
+        return Dish.menu.stream().collect(reducing(0,Dish::getCalories,Integer::sum));
     }
 }
