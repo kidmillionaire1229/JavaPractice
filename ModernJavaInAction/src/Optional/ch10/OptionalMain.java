4131d7bd87bdf1368d3b7e637e8a447362f773c2
minjun <ijmtylhtf@naver.com>
2023-08-14 21:02:57 +0900

    [Feat] Optional.empty()로 빈 Optional 객체 반환

---
+++ ModernJavaInAction/src/Optional/ch10/OptionalMain.java
@@ -0,0 +1,11 @@
+package Optional.ch10;
+
+import java.util.Optional;
+
+public class OptionalMain {
+
+    public static void main(String[] args) {
+        //Optional.empty (정적 팩토리 메서드) 로 빈 객체를 얻을 수 있다.
+        Optional<Car> empty = Optional.empty();
+    }
+}
