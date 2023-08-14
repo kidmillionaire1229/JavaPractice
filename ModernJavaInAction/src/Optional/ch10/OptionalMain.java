8d0b1f6ae0a68ad94311563b932b121f726ee12b
minjun <ijmtylhtf@naver.com>
2023-08-14 21:04:24 +0900

    [Feat] Optional.of 사용

--- ModernJavaInAction/src/Optional/ch10/OptionalMain.java
+++ ModernJavaInAction/src/Optional/ch10/OptionalMain.java
@@ -7,5 +7,9 @@ public class OptionalMain {
     public static void main(String[] args) {
         //Optional.empty (정적 팩토리 메서드) 로 빈 객체를 얻을 수 있다.
         Optional<Car> empty = Optional.empty();
+
+        //Optional.of로 null이 아닌 값을 포함하는 Optional 만듬
+        Car car = new Car();
+        Optional<Car> car2 = Optional.of(car);
     }
 }
