d95f9fff0c14420752fe02564249f7477c28e378
minjun <ijmtylhtf@naver.com>
2023-08-15 15:07:40 +0900

    [Add] 예시 데이터 추가 및 filterGreenApples 호출

--- ModernJavaInAction/src/람다/ch2/FilteringApples.java
+++ ModernJavaInAction/src/람다/ch2/FilteringApples.java
@@ -1,9 +1,19 @@
 package 람다.ch2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FilteringApples {
+
+    public static void main(String[] args) {
+        List<Apple> inventory = Arrays.asList(
+                new Apple(80, Color.GREEN),
+                new Apple(150, Color.GREEN),
+                new Apple(120, Color.RED));
+
+        System.out.println("filtering GreenApples : "+filterGreenApples(inventory));
+    }
 
     //1. 녹색 사과 필터링
     //가장 기본적인 방법 : 특정 색과 동일한지 비교
@@ -14,7 +24,7 @@ public class FilteringApples {
                 result.add(apple);
             }
         }
-        return result; 
+        return result;
     }
 
 }
